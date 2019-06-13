46ef441663d32074e1d192794981f57072bb6c57
cllorca1 <carlos.llorca@tum.de>
2019-06-13 09:00:26 +0200

    first assignment of hbefa types from osm (all URB?)

--- src/main/java/de/tum/bgu/msm/networkEdition/AssignHbefaRoadTypes.java
+++ src/main/java/de/tum/bgu/msm/networkEdition/AssignHbefaRoadTypes.java
@@ -1,5 +1,6 @@
 package de.tum.bgu.msm.networkEdition;
 
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
@@ -14,10 +15,35 @@ import java.util.Set;
 
 public class AssignHbefaRoadTypes {
 
-    static String inputFile = "./networks/matsim/regensburg_multimodal.xml";
+    private static final Logger logger = Logger.getLogger(AssignHbefaRoadTypes.class);
 
+    static String inputFile = "./networks/matsim/regensburg_multimodal.xml";
     static String outputFile = "./networks/matsim/regensburg_multimodal_compatible_emissions.xml";
 
+/**
+    * handled OSM road types:
+            *    motorway,trunk,primary,secondary, tertiary, unclassified,residential,service
+ *    motorway_link, trunk_link,primary_link, secondary_link
+ *    tertiary_link, living_street, pedestrian,track,road
+ *
+            * Hbefa categories and respective speeds
+            *    URB/MW-Nat./80 - 130
+            *    URB/MW-City/60 - 110
+            *    URB/Trunk-Nat./70 - 110
+            *    URB/Trunk-City/50 - 90
+            *    URB/Distr/50 - 80
+            *    URB/Local/50 - 60
+            *    URB/Access/30 - 50
+            *
+            * Conversions from OSM to hbefa types
+ *    motorway;MW
+ *    primary;Trunk
+ *    secondary;Distr
+ *    tertiary;Local
+ *    residential;Access
+ *    living;Access
+
+    **/
 
     public static void main(String args[]) {
 
@@ -37,7 +63,56 @@ public class AssignHbefaRoadTypes {
 
     private static String getHbefaType(Link link) {
 
-        return "URB/Local/50";
+        double freeFlowSpeed_kmh = link.getFreespeed()*3.6;
+        String osmType = link.getAttributes().getAttribute("type").toString().split("_")[0];
+
+        String type = "";
+        String speedRange= "";
+
+        if (osmType.equals("motorway")){
+            if (freeFlowSpeed_kmh > 80) {
+                type = "MW-Nat.";
+                speedRange = "80";
+            } else if (freeFlowSpeed_kmh > 60){
+                type = "MW-City";
+                speedRange = "60";
+            } else {
+                logger.error("Road classified with lower cat: " + osmType + " - " + freeFlowSpeed_kmh);
+                type = "Distr";
+                speedRange = "50";
+            }
+        } else if (osmType.equals("primary") || osmType.equals("trunk")){
+            if (freeFlowSpeed_kmh >= 70) {
+                type = "Trunk-Nat.";
+                speedRange = "70";
+            } else if (freeFlowSpeed_kmh >= 50){
+                type = "Trunk-City";
+                speedRange = "50";
+            } else {
+                logger.error("Road classified with lower cat: " + osmType + " - " + freeFlowSpeed_kmh);
+                type = "Distr";
+                speedRange = "50";
+            }
+        } else if (osmType.equals("secondary")){
+            type = "Distr";
+            speedRange = "50";
+        } else if (osmType.equals("tertiary")){
+            type = "Local";
+            speedRange = "50";
+        } else if (osmType.equals("residential") ||
+                osmType.equals("access") ||
+                osmType.equals("service") ||
+                osmType.equals("unclassified")){
+            type = "Access";
+            speedRange = "30";
+        } else {
+            logger.error(osmType + " - " + freeFlowSpeed_kmh);
+            throw new RuntimeException("road type not known");
+        }
+
+
+
+        return "URB/" + type + "/" + speedRange;
     }
 
 }
