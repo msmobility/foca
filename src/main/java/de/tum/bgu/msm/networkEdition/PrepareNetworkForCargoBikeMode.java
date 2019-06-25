5a8dfb284dfe43182a2c8b177f1348657a116e9e
cllorca1 <carlos.llorca@tum.de>
2019-06-25 16:40:08 +0200

    more systematically add links for bike network only

--- src/main/java/de/tum/bgu/msm/networkEdition/PrepareNetworkForCargoBikeMode.java
+++ src/main/java/de/tum/bgu/msm/networkEdition/PrepareNetworkForCargoBikeMode.java
@@ -1,6 +1,8 @@
 package de.tum.bgu.msm.networkEdition;
 
+import de.tum.bgu.msm.util.MitoUtil;
 import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
@@ -13,6 +15,10 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,12 +29,13 @@ public class PrepareNetworkForCargoBikeMode {
     static String inputFile = "./networks/matsim/final_V5_emissions.xml.gz";
     static String outputFile = "./networks/matsim/final_V7_emissions.xml.gz";
 
+    static String newNodeFileName = "./networks/input/cargo_bike_new_network/new_nodes_cargo_bike_regensburg.csv";
+    static String newLinkFileName = "./networks/input/cargo_bike_new_network/new_links_cargo_bike_regensburg.csv";
+
     static int counter = 0;
     static Set<String> modes = new HashSet<>();
 
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args) throws IOException {
 
         Config config = ConfigUtils.createConfig();
         config.network().setInputFile(inputFile);
@@ -41,27 +48,62 @@ public class PrepareNetworkForCargoBikeMode {
 
         modes.add(TransportMode.car);
 
-        //small demonstration example for regensburg
-        String[] nodesOrigin = new String[]{"33160296","1177219337","38433131"};
-        String[] nodesDestination =  new String[]{"247883701","33210235","49511778"};
+        BufferedReader br = new BufferedReader(new FileReader(newNodeFileName));
+        String[] header =  br.readLine().split(",");
+        int idIndex = MitoUtil.findPositionInArray("NODE",header);
+        int xIndex = MitoUtil.findPositionInArray("xcoord",header);
+        int yIndex = MitoUtil.findPositionInArray("ycoord",header);
 
-        for (int i = 0; i < nodesOrigin.length; i++){
-            String fromNodeId = nodesOrigin[i];
-            String toNodeId = nodesDestination[i];
-            createAndAddLinkForCargoBikeOnly(network, fromNodeId, toNodeId);
+        String line;
+        while( (line = br.readLine()) != null){
+            createAndAddNode(network,
+                    line.split(",")[idIndex],
+                    Double.parseDouble(line.split(",")[xIndex]),
+                    Double.parseDouble(line.split(",")[yIndex]));
         }
+
+        br.close();
+        logger.info("Completed the reading of nodes");
+
+        br = new BufferedReader(new FileReader(newLinkFileName));
+        header =  br.readLine().split(",");
+        int linkIdIndex = MitoUtil.findPositionInArray("LINK",header);
+        int nodeIdIndex = MitoUtil.findPositionInArray("NODE",header);
+
+        //todo not tested completely!
+        while( (line = br.readLine()) != null){
+            String line2 = br.readLine();
+
+            String fromNodeId = line.split(",")[nodeIdIndex];
+            String toNodeId = line2.split(",")[nodeIdIndex];
+
+            String linkId;
+            if ((linkId = line.split(",")[linkIdIndex]).equals(line2.split(",")[linkIdIndex])){
+                createAndAddLinkForCargoBikeOnly(network, linkId, fromNodeId, toNodeId);
+            } else {
+                throw new RuntimeException("The file of links is not well formatted");
+            }
+
+        }
+
+        logger.info("Completed the reading of links");
 
         new NetworkWriter(network).write(outputFile);
 
     }
 
-    private static void createAndAddLinkForCargoBikeOnly(Network network, String fromNodeId, String toNodeId){
+    private static void createAndAddNode(Network network, String id, double x, double y){
+        NetworkUtils.createAndAddNode(network, Id.createNodeId(id), new Coord(x,y));
+
+    }
+
+    private static void createAndAddLinkForCargoBikeOnly(Network network, String linkId, String fromNodeId, String toNodeId){
 
         counter ++;
 
         Node fromNode = network.getNodes().get(Id.createNodeId(fromNodeId));
         Node toNode = network.getNodes().get(Id.createNodeId(toNodeId));
-        Link link = NetworkUtils.createLink(Id.createLinkId("cargoBikeOnly_" + counter), fromNode,
+        Link link = NetworkUtils.createLink(Id.createLinkId("cb-" + linkId), fromNode,
                 toNode, network, NetworkUtils.getEuclideanDistance(fromNode.getCoord(), toNode.getCoord()),
                 30/3.6, 2000, 2);
         link.getAttributes().putAttribute("onlyCargoBike", true );
@@ -70,9 +112,8 @@ public class PrepareNetworkForCargoBikeMode {
 
         network.addLink(link);
 
-        counter++;
 
-        Link link_2 = NetworkUtils.createLink(Id.createLinkId("cargoBikeOnly_" + counter), toNode,
+        Link link_2 = NetworkUtils.createLink(Id.createLinkId("cb-" + linkId + "-2"), toNode,
                 fromNode, network, NetworkUtils.getEuclideanDistance(fromNode.getCoord(), toNode.getCoord()),
                 30/3.6, 2000, 2);
         link_2.getAttributes().putAttribute("onlyCargoBike", true );
